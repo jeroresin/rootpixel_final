d2ee0107c60c90736838d8770e5bcdc8f198015a
jeroresin <mailsirogane@gmail.com>
2019-08-17 12:02:28 +0700

    Update welcome.blade.php
    change page menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,11 +85,11 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://rootpixel.net">Webpage</a>
+                    <a href="https://rootpixel.net/contact">COntact</a>
+                    
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/jeroresin/rootpixel_final">GitHub</a>
                 </div>
             </div>
         </div>
